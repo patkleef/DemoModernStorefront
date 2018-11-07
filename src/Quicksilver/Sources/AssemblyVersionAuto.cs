5e825dac199ad116c85311bf7598230a39cf8f62
patkleef <patrick@geta.no>
2018-11-07 08:12:35 +0100

    Initial commit Quicksilver demo site

---
+++ src/Quicksilver/Sources/AssemblyVersionAuto.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+
+
+[assembly: AssemblyCopyright("Copyright ? 2018 Episerver")]
+[assembly: AssemblyVersion("12.11.0.0")]
+[assembly: AssemblyInformationalVersion("12.11.0")]
+[assembly: AssemblyFileVersion("12.11.0.3395")]
