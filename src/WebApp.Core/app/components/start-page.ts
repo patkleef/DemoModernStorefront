5d07e167228a4d2cd32bece5af738636016675cb
Patrick van Kleef <patrick.van.kleef@gmail.com>
2019-01-08 19:04:02 +0100

    Fix introduction tour slideshow

--- src/WebApp.Core/app/components/start-page.ts
+++ src/WebApp.Core/app/components/start-page.ts
@@ -5,6 +5,40 @@ import { repositoryFactory } from "../repositories/repositoryFactory";
 import { EventTypes } from "../models/EventTypes";
 import { trackingFactory } from "../repositories/trackingFactory";
 
+ko.bindingHandlers.slick = {
+  init: function (element, valueAccessor, allBindingsAccessor) {
+    $(element).empty();
+    var items = ko.unwrap(valueAccessor());
+    if (items) {
+      items.forEach(function (item) {
+        const div = $('<div>');
+        const strong = $('<strong>');
+        const p = $("<p>");
+        p.html(item.mainBody.value);
+        strong.html(item.title.value);
+        const image = $('<img>');
+        image.attr('src', item.mainImage.value);
+        div.append(strong);
+        div.append(p);
+        div.append(image);
+        $(element).append(div)
+      });
+    }
+
+    var options = allBindingsAccessor().slickOptions || {};
+
+    $(element).slick(options);
+
+    ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+      $(element).slick('unslick');
+    });
+  },
+
+  update: function (element, valueAccessor) {
+    var images = ko.unwrap(valueAccessor());
+  }
+};
+
 export class StartPageViewModel {
   repository = repositoryFactory.get();
   tracking = trackingFactory.get();
@@ -53,12 +87,5 @@ export class StartPageViewModel {
 
   skipIntroductionTour = () => {
     this.showIntroductionTour(false);
-  };
-
-  renderComplete = () => {
-    debugger;
-    $(".single-item").slick({
-      dots: true
-    });
   };
 }
